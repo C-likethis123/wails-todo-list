3d27f3abb3d344084e03f20610c34dacfb3a209f
Chow Jia Ying <chowjiaying211@gmail.com>
2020-08-22 12:13:15 +0800

    Conditionally render todo list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./assets/app.css";
+import "./assets/base.css";
 
 function App() {
+  const todos = [];
   return (
     <div>
       <section class="todoapp" v-cloak>
         <header class="header">
           <h1>todos</h1>
-          <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" />
+          <input
+            class="new-todo"
+            autofocus
+            autocomplete="off"
+            placeholder="What needs to be done?"
+          />
         </header>
-        <section class="main">
-          <ul class="todo-list">
-            <li class="todo">
-              <div class="view">
-                <label>A todo item</label>
-              </div>
-            </li>
-          </ul>
-        </section>
+        {todos.length > 0 ? (
+          <section class="main">
+            <ul class="todo-list">
+              <li class="todo">
+                <div class="view">
+                  <label>A todo item</label>
+                </div>
+              </li>
+            </ul>
+          </section>
+        ) : null}
       </section>
-  </div>
+    </div>
   );
 }
 
