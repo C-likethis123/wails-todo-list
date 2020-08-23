abf3d01e6324c295385f7886e26f39757a9f200c
Chow Jia Ying <chowjiaying211@gmail.com>
2020-08-23 11:00:38 +0800

    Add todo items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,24 +30,53 @@ const ToDo = ({ todo }) => {
 };
 
 function App() {
-  const todos = [{ id: 0, title: "My test to do item", completed: true }];
+  const [todos, setTodos] = useState([
+    { id: 0, title: "My test to do item", completed: false },
+  ]);
+  const [newToDo, setNewToDo] = useState("");
+
+  const detectEnterKeyPress = (event) => {
+    if (event.key === "Enter") {
+      // Add a ToDo to the todos list
+      const trimmedValue = newToDo.trim();
+      if (trimmedValue) {
+        setTodos(
+          todos.concat({
+            id: todos.length,
+            title: trimmedValue,
+            completed: false,
+          })
+        );
+      }
+      // Reset input
+      setNewToDo("");
+    }
+  };
+
+  const removeToDo = (todoId) => {
+    setTodos(todos.filter(item => item.id !== todoId))
+  }
+
   return (
     <div>
       <section class="todoapp" v-cloak>
         <header class="header">
           <h1>todos</h1>
           <input
-            class="new-todo"
+            className="new-todo"
             autofocus
             autocomplete="off"
             placeholder="What needs to be done?"
+            value={newToDo}
+            onChange={(event) => setNewToDo(event.target.value)}
+            onKeyUp={detectEnterKeyPress}
           />
         </header>
         {todos.length > 0 ? (
           <section class="main">
             <ul class="todo-list">
               {todos.map((todo) => (
-                <ToDo todo={todo} />
+                <ToDo todo={todo} removeToDo={removeToDo} />
               ))}
             </ul>
           </section>
