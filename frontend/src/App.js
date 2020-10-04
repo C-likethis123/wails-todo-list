3954479680ee966cb29cc7299664e40a4e54133b
Chow Jia Ying <chowjiaying211@gmail.com>
2020-10-04 20:53:29 +0800

    Add a title to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
-
+import './assets/app.css';
+import './assets/base.css';
 import classnames from "classnames";
 
 function App() {
   const [todos, setTodos] = useState([]);
   return (
-    <div>
+    <div className="todoapp">
+      <h1>To Do List</h1>
     </div>
   );
 }
