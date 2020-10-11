a247e265cda289a0613d5c7776be0e9bdaf19c56
Chow Jia Ying <chowjiaying211@gmail.com>
2020-10-11 17:13:45 +0800

    Rewrite backend to use ToDo structs

--- main.go
+++ main.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path"
+	"log"
 
 	"github.com/leaanthony/mewn"
 	"github.com/wailsapp/wails"
 )
 
-func saveList(todos string) error {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	filename := path.Join(cwd, "mylist.json")
-	return ioutil.WriteFile(filename, []byte(todos), 0600)
-}
-
-func loadList() (string, error) {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return "", err
-	}
-	filename := path.Join(cwd, "mylist.json")
-	bytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		err = fmt.Errorf("Unable to open list: %s", filename)
-	}
-	var result = string(bytes)
-	return result, err
-}
-
 func main() {
 
 	js := mewn.String("./frontend/build/static/js/main.js")
 	css := mewn.String("./frontend/build/static/css/main.css")
+
+	myTodoList, err := NewTodos()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app := wails.CreateApp(&wails.AppConfig{
 		Width:  1024,
@@ -47,7 +26,6 @@ func main() {
 		Colour: "#131313",
 	})
 
-	app.Bind(saveList)
-	app.Bind(loadList)
+	app.Bind(myTodoList)
 	app.Run()
 }
