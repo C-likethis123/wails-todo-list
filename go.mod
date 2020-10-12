34fa79d7074c21083509ee87e49d7f4d568613f7
Chow Jia Ying <chowjiaying211@gmail.com>
2020-10-13 00:50:44 +0800

    Emit filemodified event

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/leaanthony/mewn v0.10.7
 	github.com/wailsapp/wails v1.7.1
+	gopkg.in/fsnotify.v1 v1.4.7
 )
--- todos.go
+++ todos.go
@@ -7,6 +7,7 @@ import (
 	"path"
 
 	"github.com/wailsapp/wails"
+	"gopkg.in/fsnotify.v1"
 )
 
 type Todos struct {
@@ -49,5 +50,39 @@ func (t *Todos) WailsInit(runtime *wails.Runtime) error {
 	t.runtime = runtime
 	t.logger = t.runtime.Log.New("Todos")
 	t.logger.Info("I'm here")
+	return t.startWatcher()
+}
+
+func (t *Todos) startWatcher() error {
+	t.logger.Info("Starting Watcher")
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					t.logger.Infof("modified file: %s", event.Name)
+					t.runtime.Events.Emit("filemodified")
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				t.logger.Error(err.Error())
+			}
+		}
+	}()
+
+	err = watcher.Add(t.filename)
+	if err != nil {
+		return err
+	}
 	return nil
 }
